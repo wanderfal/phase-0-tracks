cf1f7967cd66ce73dd7316f4431b15b53e8f4b03
Kane Lee <kane.m.lee@gmail.com>
2016-10-27 17:00:31 -0400

    Release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+#Kane Lee
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+dob = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+order = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+
