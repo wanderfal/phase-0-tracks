9bd527bfb6e942daa55d02ddf8e7c3f09f28ca6a
Kane Lee <kane.m.lee@gmail.com>
2016-11-04 12:37:58 -0400

    Bubble Sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -17,46 +17,24 @@ end
 
 
 
-arr = [7, 12, 24, 87]
+# arr = [7, 12, 24, 87]
 
 
-p search_array(arr, 7)
-p search_array(arr, 100)
+# p search_array(arr, 7)
+# p search_array(arr, 100)
 
 # Release 1
 # Fibonacci
 
-# def sum(x, y)
-# 	x + y
-# end
-
-# x = output[num-2]
-# y = output[num-3]
-# Output should be an array.
-# To verify, check last number in array.
-# def fib(num)
-# 	i = 2
-# 	sum = [0,1]
-# 	output = []
-# 	while i < num
-# 		if i == 0 || i == 1
-# 			output << i
-# 			i += 1
-# 		else
-# 			x = output[num-2]
-# 			y = output[num-3]
-# 			output << sum(x, y)
-# 			i += 1
-# 		end
-# 	end
-# 	output
-# 	p output[num-1]
-# end
 
 def fib(num)
 #   i starts at 2 because in a Fibonacci sequence we start with [0,1]
 #   in the array. Which means that the next number in the series would
 #   be at index position 2.
+#   We can include an if statement to take care the cases where num is
+#   equal to a 1 or a 2.
+#   But here we assume that the first two in the Fibonacci series is given
+#   to us.
 	i = 2
 	output = [0,1]
 	while i < num
@@ -71,20 +49,52 @@ end
 
 
 
-# def fib(n, cache = {})
-#   if n == 0 || n == 1
-#   return n
-#   end
-#   cache[n] ||= fib(n-1, cache) + fib(n-2, cache)
-# end
-# fib 14
-# # => 377
-# fib 24
-# # => 46368
-# fib 124
-# # => 36726740705505779255899443
+# p fib(6)
+# p fib(100)
 
-p fib(6)
-# => [0,1,1,2,3,5]
-# => [0,1,2,3,4,5]
-p fib(100)
+
+# Release 2
+# Sorting an Array
+
+# Bubble Sort
+
+# In this sorting algorithm, we 'bubble' the element in the array that we want
+# to the end of the array. We can achieve bubble sort by taking a value and comparing
+# it with the previous value; if the previous value is greater than our current value,
+# we swap their positions. Then we move to the next value and compare it
+# with the value in the previous position. We continue doing this until we 
+# reach the last position, where we then go back the the second position and we
+# repeat the algorithm. This way, the element/value that we want/is greater has 
+# 'bubbled' to the top.
+
+
+def bubble_sort(arr)
+
+  swapped = false
+  while swapped == false
+# We change swapped to true here, so our loop knows when to
+# end itself
+    swapped = true
+# We use each_with_index since we need the position
+# We can also use each_index which only returns the index
+### arr.each_index do |i|
+    arr.each_with_index do |value, i|
+      if i == 0
+      	next
+# Once this condition is no longer true, which means I can
+# not sort anymore, then the swapped will continue to be
+# true, which will end the loop
+##### elsif arr[i-1] > arr[i]
+      elsif arr[i-1] > value
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+        swapped = false
+      end
+    end
+  end
+    arr
+end
+
+
+# arr = [15,2,4,62,14,10,1]
+
+# p bubble_sort(arr)
