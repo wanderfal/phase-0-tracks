a0e252cffbcb372b18e5addd90dc85f32897a414
Kane Lee <kane.m.lee@gmail.com>
2016-11-07 02:00:20 -0500

    Added user input

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -17,6 +17,8 @@
 
 
 def next_vowel(vowel)
+
+##Remember that 'a' is not the same as 'A'
 	vowels = ['a','e','i','o','u']
 	i = vowels.index (vowel)
 	if vowel == 'u'
@@ -29,30 +31,28 @@ end
 def next_char(char)
 	vowels = ['a','e','i','o','u']
 	if vowels.include? (char)
-			next_vowel(char)
+		next_vowel(char)
 	elsif char == 'z'
-			char = 'a'
+		char = 'a'
 	else
-			if vowels.include? (char.next)
-				char.next.next
-			else
-				char.next
-			end
+		if vowels.include? (char.next)
+			char.next.next
+		else
+			char.next
+		end
 	end
-	# if vowels.include? (char)
-	# 	char.next
-	# end
 end
 
 def encrypt(str)
 	output = ""
-	names = str.split(" ")
+	names = str.downcase.split(" ")
 	swapped = (names[0], names[1] = names[1], names[0])
 	last_name = swapped[0].split("")
 	first_name = swapped[1].split("")
 	last_name.map!{ |letter| next_char(letter) }
 	first_name.map!{ |letter| next_char(letter) }
-	output = last_name.join("") + " " + first_name.join("")
+	output = last_name.join("").capitalize + " " + first_name.join("").capitalize
+	output
 end
 
 
@@ -60,6 +60,36 @@ end
 # p next_vowel('u')
 # p next_vowel('o')
 
+# p next_char('d')
+# p next_char('n')
+
+# p encrypt("Kane Lee")
+
+# Release 1
+def cap_first(str)
+	str.split(' ').map!{ |word| word.capitalize }.join(' ')
+end
+
+# p cap_first('kane lee')
 
 
-p encrypt("Kane Lee")
+fake_names = {}
+name = "Name Here"
+loop do
+	puts "Please enter a first name and last name in that order. Type 'quit' to end."
+	name = cap_first(gets.chomp.downcase)
+	if name.empty? || name == 'Quit'
+		break
+	else
+		fake_names[name] = encrypt(name)
+	end
+end
+
+if fake_names.empty?
+	puts "You did not enter any information. You have no name. Goodbye."
+else
+	fake_names.each do |name, fake_name|
+		puts "Thank you #{fake_name}, formerly known as #{name}."
+	end
+end
+
