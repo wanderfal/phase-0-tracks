c10ee73d7c86ee6f18f1e2fb23c464ff3e32de9d
Kane Lee <kane.m.lee@gmail.com>
2016-11-06 23:16:43 -0500

    Initial Commit PseudoC

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,11 @@
+# Release 0
+# We first take the given name and swap the positions
+# of the First name, and last name.
+#
+# Then we first find all the vowels (a,e,i,o,u) within the names
+# and change them to the next vowel (a,e,i,o,u), so if you have 'u'
+# it will change to an 'a'.
+#
+# After the vowels have been changed, we then go through
+# all the other characters and change them to the next
+# character.
