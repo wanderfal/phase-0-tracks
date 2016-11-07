eca2f648a510d29254287a4220c6a7d8c3e047fd
Kane Lee <kane.m.lee@gmail.com>
2016-11-06 21:42:16 -0500

    Finished

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,51 @@
+dept_store = {
+							first_floor: {
+															electronics: [
+																						"Televisions",
+																						"Washing Machines",
+																						"Dryers",
+																						"Ranges",
+																						"Microwaves"
+															],
+															hardware: [
+																					"Lawnmowers",
+																					"Power Tools",
+																					"Outdoor Lighting",
+															],
+															cust_serv: "Pick-up Area",
+							},
+							second_floor: {
+															children: {
+																					boys: [
+																									"shoes",
+																									"shirts",
+																									"pants"
+																					],
+																					girls: [
+																									"shoes",
+																									"shirts",
+																									"pants",
+																									"dresses"
+																					]
+															},
+															mens: [
+																			"shoes",
+																			"shirts",
+																			"pants"
+															]
+
+							},
+							third_floor: {
+															womens: [
+																				"shoes",
+																				"shirts",
+																				"pants",
+																				"dresses"
+															]
+							}
+}
+
+
+p dept_store[:third_floor][:womens][3]
+p dept_store[:second_floor][:children][:boys][0]
+p dept_store[:first_floor][:cust_serv]
