a1358d42dfb9cde098e63b7d893cab4eae499125
Kane Lee <kane.m.lee@gmail.com>
2016-11-08 10:23:53 -0500

    Initial Commit Ben

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,33 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  def speak(int)
+  	int.times do
+  		puts "Woof!"
+  	end
+  end
+  def roll_over
+  	return "*rolls over*"
+  end
+  def dog_years(int)
+  	int*7
+  end
+  def walked
+  	return "Has it been walked?"
+  end
 end
 
+block = Puppy.new
+
+
+
+
+p block.fetch("toy")
+
+p block.speak(4)
+
+p block.roll_over
+
+p block.dog_years(2)
+
+p block.walked
