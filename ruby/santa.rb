ffb94e5465e4de1a9dc5474f47cd07e41cd3ea9b
Kane Lee <kane.m.lee@gmail.com>
2016-11-12 13:40:46 -0500

    Release 0

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,15 +3,22 @@ class Santa
 	
 
 	def initialize
+		puts "Initializing Santa instance..."
 	end
 
 	def speak
-
+		puts "Ho, ho, ho! Happy holidays!"
 	end
 
-	def eat_milk_and_cookies
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie} cookie!"
 	end
-
-
 
 end
+
+
+santa1 = Santa.new
+
+p santa1.speak
+
+p santa1.eat_milk_and_cookies('chocolate chip')
