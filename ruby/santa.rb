30d36036191a102a3f6a5ced5effb191d8720191
Kane Lee <kane.m.lee@gmail.com>
2016-11-12 18:10:39 -0500

    Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,8 +4,8 @@ class Santa
 
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
-		p @gender = gender
-		p @ethnicity = ethnicity
+		@gender = gender
+		@ethnicity = ethnicity
 		@reindeer_ranking = [
 			'Rudolph',
 			'Dasher',
@@ -28,6 +28,33 @@ class Santa
 		puts "That was a good #{cookie} cookie!"
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(r_name)
+		@reindeer_ranking << @reindeer_ranking.delete(r_name)
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
+	def gender
+		@gender
+	end
 end
 
 
@@ -44,7 +71,17 @@ genders = ['male', 'female', 'other']
 ethnicities = ['white', 'black', 'orange', 'yellow', 'pink', 'green']
 
 
-ethnicities.each do |ethnicity|
-	santas << Santa.new(genders.sample, ethnicity)
-end
-
+# ethnicities.each do |ethnicity|
+# 	santas << Santa.new(genders.sample, ethnicity)
+# end
+# Sample may not be the best method when testing...
+p santa2 = Santa.new(genders.sample, ethnicities.sample)
+puts "This Santa was #{santa2.age} years old, but today is their birthday. So now this Santa is #{santa2.celebrate_birthday} years old!"
+puts "This Santa was a #{santa2.gender}."
+santa2.gender = 'supergender?'
+puts "After an accident, this Santa became a #{santa2.gender}!"
+p santa2.reindeer_ranking
+santa2.get_mad_at('Dancer')
+puts "Dancer cut in line, so they were sent to the back."
+p santa2.reindeer_ranking
+puts "This Santa is of #{santa2.ethnicity} ethnicity!"
