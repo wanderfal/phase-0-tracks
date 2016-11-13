3938a57704378553b9927da846f89d39f42b9a30
Kane Lee <kane.m.lee@gmail.com>
2016-11-12 23:10:31 -0500

    comment

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -97,43 +97,43 @@ update_num(another_list, 'Ice Cream', 1)
 p another_list
 puts pretty_list(another_list)
 
-# Release 4 Reflect
-What did you learn about pseudocode from working on this challenge?
-  I learned that there are many ways to approach pseudocoding.
-  You generally look at a problem and pseudocode the entire thing
-  before you attempt to code. However, I find that, personally, it
-  was more effective to give myself a general idea of what type of
-  input I will need, and the expected output from the problem. Then
-  as I move through the problem, I continue to pseudocode each piece
-  of the problem, and thoroughly flesh out the problem; compared to
-  pseudocoding the entire thing at the beginning and possibly missing
-  some elements.
+# # Release 4 Reflect
+# What did you learn about pseudocode from working on this challenge?
+#   I learned that there are many ways to approach pseudocoding.
+#   You generally look at a problem and pseudocode the entire thing
+#   before you attempt to code. However, I find that, personally, it
+#   was more effective to give myself a general idea of what type of
+#   input I will need, and the expected output from the problem. Then
+#   as I move through the problem, I continue to pseudocode each piece
+#   of the problem, and thoroughly flesh out the problem; compared to
+#   pseudocoding the entire thing at the beginning and possibly missing
+#   some elements.
 
-What are the tradeoffs of using arrays and hashes for this challenge?
-  In the case of using a data structure for a list containing an item
-  and a specified quantity, hashes were perfect for this. Arrays would
-  be less efficient, since we do not utilize any type of a order in the
-  list.
+# What are the tradeoffs of using arrays and hashes for this challenge?
+#   In the case of using a data structure for a list containing an item
+#   and a specified quantity, hashes were perfect for this. Arrays would
+#   be less efficient, since we do not utilize any type of a order in the
+#   list.
 
-What does a method return?
-  It generally returns the last line of code in the method. Or when the
-  method is prompted to before the end, such as using 'return'.
+# What does a method return?
+#   It generally returns the last line of code in the method. Or when the
+#   method is prompted to before the end, such as using 'return'.
 
-What kind of things can you pass into methods as arguments?
-  Depends on the method. Some methods will take in all objects, which
-  means it will take any type of argument. Some take in only strings, or
-  data structures.
+# What kind of things can you pass into methods as arguments?
+#   Depends on the method. Some methods will take in all objects, which
+#   means it will take any type of argument. Some take in only strings, or
+#   data structures.
 
-How can you pass information between methods?
-  Assigning that information to variables that can be used throughout.
+# How can you pass information between methods?
+#   Assigning that information to variables that can be used throughout.
 
-What concepts were solidified in this challenge, and what concepts are
-still confusing?
-  Default values and working with methods and their arguments, were
-  definitely solidified during this session. My understanding between
-  Arrays and Hashes had also improved significantly. I was also able to
-  gain a greater understanding on how ruby handles returns and how I can
-  incorporate that into my code. I did not find any concept in this session
-  to be too difficult to comprehend. Some concepts did prove confusing 
-  during the sessions. However, with the guide, I was able to understand
-  those concepts.
+# What concepts were solidified in this challenge, and what concepts are
+# still confusing?
+#   Default values and working with methods and their arguments, were
+#   definitely solidified during this session. My understanding between
+#   Arrays and Hashes had also improved significantly. I was also able to
+#   gain a greater understanding on how ruby handles returns and how I can
+#   incorporate that into my code. I did not find any concept in this session
+#   to be too difficult to comprehend. Some concepts did prove confusing 
+#   during the sessions. However, with the guide, I was able to understand
+#   those concepts.
