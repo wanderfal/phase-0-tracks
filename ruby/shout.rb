461ac940d9b4b2c246649d94f8b0fe79a034a2ca
Kane Lee <kane.m.lee@gmail.com>
2016-11-12 21:37:00 -0500

    Release 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-	
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+	def self.yelling_happily(words)
+		words + "!!" + " :)"
+	end
 end
+
+p Shout.yell_angrily("Help, I have fallen and can't get up")
+
+p Shout.yelling_happily("Hello, the weather today is nice")
