55acf09a156c95395b74a70eae78223b270f5497
Kane Lee <kane.m.lee@gmail.com>
2016-11-12 21:48:57 -0500

    Release 3 finished

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+# 	def self.yelling_happily(words)
+# 		words + "!!" + " :)"
+# 	end
+# end
+
+# p Shout.yell_angrily("Help, I have fallen and can't get up")
+
+# p Shout.yelling_happily("Hello, the weather today is nice")
+
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		words + "!!" + " :)"
 	end
 end
 
-p Shout.yell_angrily("Help, I have fallen and can't get up")
+class Senior
+	include Shout
+end
 
-p Shout.yelling_happily("Hello, the weather today is nice")
+class Children
+	include Shout
+end
+
+senior = Senior.new
+p senior.yell_angrily('Get off my lawn')
+p senior.yelling_happily('Get off my lawn')
+
+child = Children.new
+p child.yell_angrily('Hello mister')
+p child.yelling_happily('Hello mister')
