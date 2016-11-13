b0752b2fbae56a945f853d76512fee757732b98d
Kane Lee <kane.m.lee@gmail.com>
2016-11-13 17:46:00 -0500

    Design Release 1

---
+++ ruby/game/word_guess.rb
@@ -0,0 +1,15 @@
+# Class
+# 2-Player Game
+# Player 1 will provide the game with the word
+# Player 2 will have a limited number of guesses based on the word's length
+# Only unique guesses will count towards the limit
+#
+# After Player 1 provides the input, Player 2 will see placeholder
+# characters indicating the amount of characters in the word. Like:
+# "_ _ _ _ _".
+#
+# Player 2 is allowed single character guesses. If the characer is
+# found in the word, then it will replace the placeholder. Aside from
+# single character guesses, all other input must be exactly equal to
+# the word or phrase.
+
