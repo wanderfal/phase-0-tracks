c4a98bd6ec0990b120187fd7a6c1e22681388bdb
Kane Lee <kane.m.lee@gmail.com>
2016-11-19 17:28:36 -0500

    Added reverse function

---
+++ js/explore.js
@@ -0,0 +1,21 @@
+// Reverse function
+// Take a string and starting from the end of the string
+// Create a new string
+
+function reverse(str) {
+	var output = "";
+	for (var i = (str.length-1); i >= 0; i--) {
+//	console.log(str[i])
+		output += str[i];
+	}
+	return output;
+}
+
+// I cannot use negative indexes to refer to the position
+// of the character in a string. It returns 'undefined'
+// However str.slice() does allow the use of negative indexes.
+
+
+console.log(reverse("Hello"));
+
+console.log(reverse("Hello World"))
