8c6d8d78dcc16e8c32ca1e8157e4f30d042a7b62
Kane Lee <kane.m.lee@gmail.com>
2016-11-21 02:59:49 -0500

    Driver Code & Formatting

--- js/algos.js
+++ js/algos.js
@@ -14,7 +14,7 @@ function longest_phrase(arr) {
 	for (var i = 0;; i++) {
 		// console.log(arr[i])
 		if (i == (arr.length)) {
-			return console.log(phrase + " is your longest phrase!");
+			return console.log(phrase + " is your longest phrase!" + '\n');
 			break;
 		} else if (phrase.length < arr[i].length) {
 			phrase = arr[i];
@@ -23,17 +23,6 @@ function longest_phrase(arr) {
 		}
 	}
 }
-
-// Release 0 Driver Code
-
-var array = ['Hello', 'Hello World', 'Kane Lee']
-longest_phrase(array)
-
-var array2 = ['thelongestwordever', 'Kane Lee', 'Hello', 'Hello World']
-longest_phrase(array2)
-
-// Release 0 Driver code
-
 
 
 // Release 1
@@ -65,21 +54,6 @@ function object_match(obj1, obj2) {
 	return console.log(false);
 }
 
-// Release 1 Driver Code
-
-var associated_array1 = {name: "Steven", age: 54, hobby: "cycling"}
-var associated_array2 = {name: "Tamir", age: 54, hobby: "baseball"}
-
-object_match(associated_array1, associated_array2)
-
-
-var objA = {fruit: "Orange", age: "50"}
-var objB = {fruit: "Banana", age: "45"}
-
-object_match(objA, objB)
-
-// Release 1 Driver Code
-
 
 // Release 2
 // Based on a randomly generated integer, the word's length
@@ -87,7 +61,6 @@ object_match(objA, objB)
 // determined length and add that string to a array.
 // We then repeat until we have enough 'words' in the array.
 // Minimum length of 1 letter to Maximum length of 10 letters.
-
 
 function rand_data(int) {
 	var chars = "abcdefghijklmnopqrstuvwxyz"
@@ -113,12 +86,38 @@ function rand_data(int) {
 	return output;
 }
 
+///////////////////////////////////////////////////////////////////
+
+// Release 0 Driver Code
+var array = ['Hello', 'Hello World', 'Kane Lee']
+longest_phrase(array)
+
+var array2 = ['thelongestwordever', 'Kane Lee', 'Hello', 'Hello World']
+longest_phrase(array2)
+// Release 0 Driver code
+
+
+// Release 1 Driver Code
+var associated_array1 = {name: "Steven", age: 54, hobby: "cycling"}
+var associated_array2 = {name: "Tamir", age: 54, hobby: "baseball"}
+object_match(associated_array1, associated_array2)
+var objA = {fruit: "Orange", age: "50"}
+var objB = {fruit: "Banana", age: "45"}
+object_match(objA, objB)
+// Release 1 Driver Code
+
+
 // Release 2 Driver Code
-
 rand_data(4)
-
 // Release 2 Driver Code
 
 // 7.3 Driver Code
 
-longest_phrase(rand_data(10))
+// longest_phrase(rand_data(10))
+
+for(var x=0; x<10; x++) {
+	console.log("This is number " + (x+1) +" !")
+	var num = Math.floor(Math.random()*10 + 2);
+	console.log("You have an array with " + num + " elements!")
+	longest_phrase(rand_data(num));
+}
