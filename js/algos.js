d5fa5e28552db755f927a0468e950a945e2d1507
Kane Lee <kane.m.lee@gmail.com>
2016-11-21 01:19:16 -0500

    Release 1

--- js/algos.js
+++ js/algos.js
@@ -36,4 +36,44 @@ longest_phrase(array2)
 
 // Release 1
 // Find Key-Value Match
-// 
+// We choose one object and compare each key to all the keys
+// in the second object.
+// If a match is found, then we compare their values.
+// If the key-value pairs match between the two objects, we
+// end the function and return 'true'.
+// Otherwise we continue looking through each key in the first
+// object and comparing it to every key in the second object
+// looking for key matches, before looking for value matches.
+
+function object_match(obj1, obj2) {
+	for (var key1 in obj1) {
+		for (var key2 in obj2) {
+			if (key1 == key2) {
+				if (obj1[key1] == obj2[key2]) {
+					return console.log(true);
+					break;
+				} else {
+					continue;
+				}
+			} else {
+				continue;
+			}
+		}
+	}
+	return console.log(false);
+}
+
+// Release 1 Driver Code
+
+var associated_array1 = {name: "Steven", age: 54, hobby: "cycling"}
+var associated_array2 = {name: "Tamir", age: 54, hobby: "baseball"}
+
+object_match(associated_array1, associated_array2)
+
+
+var objA = {fruit: "Orange", age: "50"}
+var objB = {fruit: "Banana", age: "45"}
+
+object_match(objA, objB)
+
+// Release 1 Driver Code
