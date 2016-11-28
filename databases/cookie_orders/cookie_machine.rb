c95de4dd1aefe631b10165e2e9890bafe12bf649
Kane Lee <kane.m.lee@gmail.com>
2016-11-28 12:52:12 -0500

    Added interface for users to add to tables

--- databases/cookie_orders/cookie_machine.rb
+++ databases/cookie_orders/cookie_machine.rb
@@ -85,3 +85,34 @@ boroughs = [
 #
 # DATA GENERATION
 
+order_num = 1
+
+puts "Hello, welcome to the Cookie Factory!"
+p "Can I take your order?"
+p "What cookie would you like?"
+p cookie_list
+cookie = gets.chomp.capitalize
+p "How many would you like?"
+count = gets.chomp.to_i
+p "Can I have your name?"
+name = gets.chomp
+p "Phone number?"
+num = gets.chomp.to_i
+p "Which borough do you live in?"
+p boroughs
+borough = gets.chomp.capitalize
+
+p "Your order is #{count} of #{cookie} cookies?y/n"
+correct = gets.chomp.downcase
+if correct == 'y'
+	p "Thank you for your order #{name} of #{borough}!"
+	cookie_choice(data, cookie, count, order_num)
+	order_create(data, name, num, borough)
+	order_num += 1
+	# data.execute("SELECT order_id, name, type, count FROM cookies JOIN orders ON
+	# 	cookies.order_id = orders.id")
+else
+	p "What would you like to change?"
+	# FEATURE
+end
+
